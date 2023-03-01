532ba4c60ae2845b249ff2a4e20c8aa5ff7dc4b2
Shane Misra <shane.misra@gmail.com>
2023-02-28 18:21:45 -0700

    test whether or not we can use API data to properly instantiate our class - it worked

--- src/scripts.js
+++ src/scripts.js
@@ -2,14 +2,17 @@ import './css/styles.css';
 import './images/turing-logo.png'
 import fetchData from '/dist/apiCalls'
 import testRoom from './data/roomsTestData';
+import Room from './classes/Room'
 // Promise retrieval:
 let customersData, roomsData, bookingsData;
+let testApiPull;
 Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
   .then((hotelArray)=> {
-    customersData = hotelArray[0];
-    roomsData = hotelArray[1];
-    bookingsData = hotelArray[2];
+    customersData = hotelArray[0].customers;
+    roomsData = hotelArray[1].rooms;
+    bookingsData = hotelArray[2].bookings;
     hotelArray.forEach(hotelAspect => console.log(hotelAspect))
+    testApiPull = new Room(roomsData[0])
     return hotelArray
   })
 
