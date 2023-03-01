db09e2afd0aa32c117da7f7c5c295fd011437c50
Shane Misra <shane.misra@gmail.com>
2023-03-01 12:40:07 -0700

    set up api fetching and class instantiation fully

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,12 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import fetchData from '/dist/apiCalls'
-import testRoom from './data/roomsTestData';
 import Room from './classes/Room'
 import Booking from './classes/Booking'
 import Customer from './classes/Customer';
 import Hotel from './classes/Hotel';
 // Promise retrieval:
-let customersData, roomsData, bookingsData, hotelData;
-let testRoomSummon, testBookingSummon, testFaustino;
+let hotelData;
 Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
   .then((data)=> {
     let apiData = {
@@ -16,10 +14,6 @@ Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
       rooms: data[1].rooms,
       bookings: data[2].bookings
     }
-    testFaustino = new Customer(apiData.customers[8])
-    testFaustino.retrieveMyBookings(apiData.bookings, apiData.rooms);
-    testFaustino.calcTotalCost();
-    console.log(testFaustino);
     return apiData
   })
   .then(allHotelData => {
@@ -29,8 +23,15 @@ Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
       thisCust.retrieveMyBookings(allHotelData.bookings, allHotelData.rooms)
       thisCust.calcTotalCost()
       return thisCust
-      // console.log(`'Welcome, ${thisCust.name}'`, thisCust)
     })
-    // hotelData.bookings.forEach(booking => booking.retrieveRoomInfo(allHotelData.rooms))
-    console.log(hotelData)
+    hotelData.bookings = hotelData.bookings.map(booking=> {
+      let thisBooking = new Booking(booking)
+      thisBooking.retrieveRoomInfo(allHotelData.rooms);
+      return thisBooking;
+    })
+    hotelData.rooms = hotelData.rooms.map(room => new Room(room));
+    console.log('All Hotel Data:', hotelData)
+    return hotelData
   })
+
+  // With this promise set up, I should be able to log in as a customer and then use the hotelData global variable in order to look up their login attempt and assign them the user info that is being properly instantiated in this second part of our fetch.
