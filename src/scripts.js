ae2fedb40b6c6d76adb9b174404610eec56edfc6
Shane Misra <shane.misra@gmail.com>
2023-02-28 18:46:19 -0700

    Room and Booking classes and basic property tests complete

--- src/scripts.js
+++ src/scripts.js
@@ -3,17 +3,19 @@ import './images/turing-logo.png'
 import fetchData from '/dist/apiCalls'
 import testRoom from './data/roomsTestData';
 import Room from './classes/Room'
+import Booking from './classes/Booking'
 // Promise retrieval:
 let customersData, roomsData, bookingsData;
-let testApiPull;
+let testRoomSummon, testBookingSummon;
 Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
   .then((hotelArray)=> {
     customersData = hotelArray[0].customers;
     roomsData = hotelArray[1].rooms;
     bookingsData = hotelArray[2].bookings;
     hotelArray.forEach(hotelAspect => console.log(hotelAspect))
-    testApiPull = new Room(roomsData[0])
+    testRoomSummon = new Room(roomsData[0])
+    testBookingSummon = new Booking(bookingsData[0])
+    console.log('api room:', testRoomSummon);
+    console.log('api booking:', testBookingSummon);
     return hotelArray
   })
-
-  console.log('this is the test room object:', testRoom[0])
