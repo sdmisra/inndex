5d780ae08de9d437ce4bb97036d4e61aa295d8bb
Shane Misra <shane.misra@gmail.com>
2023-02-28 18:11:03 -0700

    set up the Room class properties and tests for basic properties

--- test/Room-test.js
+++ test/Room-test.js
@@ -1,13 +1,44 @@
-import chai from 'chai';
+import {assert, expect} from 'chai';
 import testRoomData from '../src/data/roomsTestData';
 import Room from '../src/classes/Room';
-const expect = chai.expect;
 
 describe('Room tests are running', function() {
-  
-  it('should return true', function() {
-    const testRoom = new Room(testRoomData)
-    expect(true).to.equal(true);
+  let testRoom;
+
+  beforeEach(()=> {
+    testRoom = new Room(testRoomData);
   })
-  ;
+
+  it('should be a function', ()=> {
+    assert.isFunction(Room);
+  })
+
+  it('should be able to create an instance of Room', () => {
+    assert.instanceOf(testRoom, Room)
+  })
+
+  it('should contain a room number', () => {
+    assert.equal(testRoom.number, 1)
+  })
+  
+  it('should contain a room type', () => {
+    assert.equal(testRoom.roomType, 'residential suite')
+  })
+
+  it('should indicate the presence of a bidet',() => {
+    assert.equal(testRoom.bidet, true)
+  })
+
+  it('should contain the size of beds in the room', () => {
+    assert.equal(testRoom.bedSize, 'queen')
+  })
+
+  it('should indicate the number of beds in the room', () => {
+    assert.equal(testRoom.numBeds, 1)
+  })
+
+  it('should indicate the the cost per night to stay in the room', () => {
+    assert.equal(testRoom.costPerNight, 358.4)
+  })
+
 });
