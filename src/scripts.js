7600cbe9724825c2946301866e1b69fbf46d814c
Shane Misra <shane.misra@gmail.com>
2023-03-03 11:00:34 -0700

    have the general shape of things, next is login page / show hide funcs

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ import Booking from './classes/Booking'
 import Customer from './classes/Customer';
 import Hotel from './classes/Hotel';
 // Promise retrieval:
-let hotelData;
+let hotelData, availableRooms;
 Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
   .then((data)=> {
     let apiData = {
@@ -35,12 +35,25 @@ Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
   })
 
   // With this promise set up, I should be able to log in as a customer and then use the hotelData global variable in order to look up their login attempt and assign them the user info that is being properly instantiated in this second part of our fetch.
+  // Current iteration is using loginCustomer() below on ~ln 60
 
   // Document Selectors //
+
   const bookRoomInput = document.querySelector('#bookingInput')
   const bookRoomBtn = document.querySelector('#bookingBtn')
+
   const userNameInput = document.querySelector('#loginName')
   const userPassInput = document.querySelector('#loginPass')
+
+  const mainBucket = document.querySelector('#mainBookingsBrowser')
+  const savedBucket = document.querySelector('#savedBookingsBrowser')
+
+  const defaultMainView = document.querySelector('#defaultMainView')
+  const filteredMainView = document.querySelector('#filteredMainView')
+  const defaultSavedView = document.querySelector('#defaultSavedView')
+  const filteredSavedView = document.querySelector('#filteredSavedView')
+
+  const rewardsWords = document.querySelector('#rewardsText')
   // Event Listeners // 
 bookRoomBtn.addEventListener('click', (event)=> {
   checkThisDate(event)
@@ -53,22 +66,40 @@ userNameInput.addEventListener('change', () => {
     let reformatClick = bookRoomInput.value.replaceAll('-', '/')
     let bookedRoomNumbers = hotelData.bookings.filter(booking=> booking.date === reformatClick).map(booking => booking.roomNumber)
     // console.log(bookedRoomNumbers)
-    let availableRooms = hotelData.rooms.filter(room => !bookedRoomNumbers.includes(room.number))
+    availableRooms = hotelData.rooms.filter(room => !bookedRoomNumbers.includes(room.number))
     console.log(`These rooms are available for ${reformatClick}`,availableRooms)
+    renderRooms(availableRooms, defaultMainView);
+    // availableRooms.forEach(room => mainBucket.innerText += room)
     // Display available rooms in the top section of the page, where a user is browsing their current options.
   }
 
   function loginCustomer() {
+    rewardsWords.innerText = ''
+    // savedBucket.innerText = ''
     let idNum = Number(userNameInput.value.split('customer')[1])
     let testCustomer = hotelData.customers.find(customer => customer.id === idNum)
     console.log(testCustomer)
-    // Display customers current and past bookings using testCustomer.bookings
     // Display customers total amount spent using testCustomer.rewardsPoints
+    rewardsWords.innerText = `You have ${testCustomer.rewardsPoints} rewards points! Thank you for your continued loyalty.`
+    let myRooms = testCustomer.bookings.map(booking=>booking.roomDetails);
+    renderRooms(myRooms, defaultSavedView)
   }
-  function displayRooms(array, destination) {
-
+  function renderRooms(array, element) {
+    element.innerHTML = ""
+    array.forEach(item => {
+      element.innerHTML += `<div class="room-card">${item}</div>`
+    })
   }
-  // function bookDate(currentUserId) {
-
+  
+  // function filterRooms(roomsArray, filterValue) {
+  //   let filtered = roomsArray.filter(room => room.roomType === filterValue)
+  //   renderRooms(filtered, mainBucket)
+  // }
+  
+  // function renderSmallView(array, element){
+    // This should be a reusable function that enables us to render the saved rooms that a user has upcoming or from past bookings. This should also be usable to filter in a similar way to renderMainView. This function and main view might end up being one larger function with a check for where the event was started.
   // }
 
+  // function bookDate(currentUserId) {
+    // This is intended to be the function that actually triggers the fetch / POST request that adds a new booking to our API endpoint.
+  // }
