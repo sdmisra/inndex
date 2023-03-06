a978f4684a8892a1bd9fc45500c42cd38b1da72a
Shane Misra <shane.misra@gmail.com>
2023-03-06 15:59:16 -0700

    filter working well, toggles need reorganization

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -8,6 +8,7 @@ class Hotel {
     this.rooms = hotelData.rooms
     this.bookings = hotelData.bookings
   }
+
   retrieveHotelInfo(hotelData) {
     this.customers = hotelData.customers.map(customer => {
       let thisCust = new Customer(customer)
@@ -23,6 +24,24 @@ class Hotel {
     this.rooms = hotelData.rooms.map(room => new Room(room));
   }
 
+  loginCustomer(num) {
+    let foundCustomer = this.customers.find(customer => customer.id === num)
+    return foundCustomer
+  }
+  
+  getAvailableRooms(date, filter = undefined) {
+    let dateToCheck = date;
+    let bookedRoomNumbers = this.bookings.filter(booking=> booking.date === dateToCheck).map(booking => booking.roomNumber)
+    let availableRooms;
+    if (filter) {
+      availableRooms = this.rooms.filter(room => !bookedRoomNumbers.includes(room.number) && room.roomType == filter)
+    }
+    else {
+      availableRooms = this.rooms.filter(room => !bookedRoomNumbers.includes(room.number))
+    }
+    this.openRooms = availableRooms;
+    return availableRooms
+  }
 }
 
 export default Hotel;
--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,9 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import fetchData from '/dist/apiCalls'
-import Room from './classes/Room'
-import Booking from './classes/Booking'
-import Customer from './classes/Customer';
 import Hotel from './classes/Hotel';
 // Promise retrieval:
-let hotelData, availableRooms, reformatDate, thisCustomer, currentBooking, saveNumber, saveId, bookedRoomNumbers;
+let hotelData, roomFilter, customerRooms, reformatDate, thisCustomer, currentBooking;
 
 function refreshData () {
    Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
@@ -21,99 +18,112 @@ function refreshData () {
   .then(allHotelData => {
     hotelData = new Hotel(allHotelData);
     hotelData.retrieveHotelInfo(allHotelData);
-    console.log(hotelData);
+    console.log('hotel updated:',hotelData);
     return hotelData
   })
 }
-refreshData();
 
   // Document Selectors //
   const loginView = document.querySelector('#loginWindow')
 
-  const bookRoomLabel = document.querySelector('#bookingLabel')
   const bookRoomInput = document.querySelector('#bookingInput')
   const roomTypeInput = document.querySelector('#roomTypeInput')
   const bookRoomBtn = document.querySelector('#bookingBtn')
+  const userBrowseButton = document.querySelector('#userBrowseButton')
   const radioBtns = document.querySelectorAll('.radio-room-button')
 
   const userNameInput = document.querySelector('#loginName')
   const userPassInput = document.querySelector('#loginPass')
   const userNav = document.querySelector('#userInfo')
-  const userToggleBookings = document.querySelector('#userBookingsBtn')
+  const userToggleBookingsBtn = document.querySelector('#userBookingsBtn')
   const userLogOut = document.querySelector('#userLogOutBtn')
   const userLogIn = document.querySelector('#userLogInBtn')
 
   const mainBucket = document.querySelector('#mainBookingsBrowser')
   const savedBucket = document.querySelector('#savedBookingsBrowser')
-  const fullPageView = document.querySelector('#full-page-view')
+  const fullPageView = document.querySelector('#fullPageView')
+  const browserView = document.querySelector('#newBookingView')
   const defaultMainView = document.querySelector('#defaultMainView')
   const defaultSavedView = document.querySelector('#defaultSavedView')
-  const filteredSavedView = document.querySelector('#filteredSavedView')
 
   const rewardsWords = document.querySelector('#rewardsText')
+  const announceWords = document.querySelector('#announceText')
   // Event Listeners // 
 
 window.addEventListener('load', () => {
-    toggleElements([fullPageView])
-    })
+  refreshData();
+  toggleElements([fullPageView])
+  })
 bookRoomBtn.addEventListener('click', (event)=> {
-  let roomFilter = undefined;
-  for (let radioButton of radioBtns) {
-    const checkedButton = radioButton.checked;
-    checkedButton ? roomFilter = radioButton.id : null
-  }
-  checkThisDate(roomFilter)
+  resetFilter();
+  checkThisDate(roomFilter);
+})
+userBrowseButton.addEventListener('click', ()=> {
+  toggleElements([browserView, bookRoomBtn]);
 })
 userNameInput.addEventListener('keypress', (event) => {
   if (event.keyCode == 13) {
   renderCustomer();
   toggleElements([loginView])
-  toggleElements([fullPageView])
+  toggleElements([fullPageView, mainBucket])
   }
 })
+
 mainBucket.addEventListener('click', (event) => {
   bookRoom(event, hotelData);
-})
-userToggleBookings.addEventListener('click', () => {
-  toggleElements([savedBucket])
+  resetFilter();
+  toggleElements([browserView, bookRoomBtn])
+  setTimeout(()=> {
+    renderCustomer();
+    toggleElements([announceWords]);
+  }, 5000)
 })
 
+userToggleBookingsBtn.addEventListener('click', () => {
+  renderCustomer();
+  toggleElements([savedBucket])
+})
+// userBrowseButton.addEventListener(
+//   // toggleElements([])
+// )
 userLogOut.addEventListener('click', () => {
   thisCustomer = undefined;
   toggleElements([loginView])
-  toggleElements([fullPageView])
+  toggleElements([fullPageView, mainBucket, savedBucket])
 })
+
 userLogIn.addEventListener('click', ()=> {
   renderCustomer();
   toggleElements([loginView])
-  toggleElements([fullPageView])
+  toggleElements([fullPageView, mainBucket])
 })
+
   // Event Handlers // 
   function checkThisDate (filter) {
     reformatDate = bookRoomInput.value.replaceAll('-', '/')
-    // 
-    bookedRoomNumbers = hotelData.bookings.filter(booking=> booking.date === reformatDate).map(booking => booking.roomNumber)
-    if (filter) {
-      availableRooms = hotelData.rooms.filter(room => !bookedRoomNumbers.includes(room.number) && room.roomType == filter)
+    let roomsForDate = hotelData.getAvailableRooms(reformatDate, filter)
+    if (!roomsForDate.length) {
+      denyBooking(reformatDate);
+      hideElements([mainBucket]);
+      setTimeout(()=> {
+        toggleElements([announceWords])
+      }, 3000)
+      return
     }
     else {
-      availableRooms = hotelData.rooms.filter(room => !bookedRoomNumbers.includes(room.number))
+      renderRooms(roomsForDate, defaultMainView);
+      showElements([mainBucket]);
     }
-    if (!availableRooms.length) {
-      alert('There are no available rooms for that date and room type! Please choose a different room type or date to keep browsing.')
-    }
-    renderRooms(availableRooms, defaultMainView);
   }
 
   function renderCustomer() {
     rewardsWords.innerText = ''
     let idNum = Number(userNameInput.value.split('customer')[1])
-    thisCustomer = hotelData.customers.find(customer => customer.id === idNum)
+    thisCustomer = hotelData.loginCustomer(idNum);
     rewardsWords.innerText = `Welcome back ${thisCustomer.name.split(' ')[0]}! You have ${thisCustomer.rewardsPoints} rewards points! Thank you for your continued loyalty.`
-    let customerRooms = thisCustomer.retrieveMyRooms();
+    customerRooms = thisCustomer.retrieveMyRooms();
     renderRooms(customerRooms, defaultSavedView)
-    toggleElements([bookRoomLabel, userNav])
-
+    toggleElements([userNav])
   }
   function renderRooms(array, element) {
     element.innerHTML = ""
@@ -131,40 +141,48 @@ userLogIn.addEventListener('click', ()=> {
     })
   }
   function bookRoom(click) {
-    saveNumber = click.target.id;
-    saveId = thisCustomer.id;
+    let saveNumber = click.target.id;
+    let saveId = thisCustomer.id;
     console.log('clicked:', saveNumber)
     currentBooking = {"userID": `${saveId}`, "date": `${reformatDate}`, "roomNumber": `${saveNumber}`}
     postRoomBooking(currentBooking);
-    checkThisDate();
-    renderCustomer();
+    confirmBooking(reformatDate, saveNumber);
+    console.log(hotelData)
   }
-  // function filterRooms(roomsArray, filterValue) {
-  //   let filtered = roomsArray.filter(room => room.roomType === filterValue)
-  //   renderRooms(filtered, mainBucket)
-  // }
-  
-  // function renderSmallView(array, element){
-    // This should be a reusable function that enables us to render the saved rooms that a user has upcoming or from past bookings. This should also be usable to filter in a similar way to renderMainView. This function and main view might end up being one larger function with a check for where the event was started.
-  // }
 
-  // function bookDate(currentUserId) {
-    // This is intended to be the function that actually triggers the fetch / POST request that adds a new booking to our API endpoint.
-  // }
+  function confirmBooking(date, room) {
+    toggleElements([announceWords])
+    toggleElements([mainBucket])
+    announceWords.innerText = `You have made a successful booking for room #${room} on ${date.replaceAll('/','-')}! We will be looking forward to your visit.`
+  }
 
-  // function show(array){
-  //   const showElements = array.map(element => element.classList.remove('hidden'));
-  //   return showElements;
-  // }
-    
-  // function hide(array) {
-  //   const hideElements = array.map(element => element.classList.add('hidden'));
-  //   return hideElements;
-  // }
+  function denyBooking(date) {
+    toggleElements([announceWords])
+    hideElements([mainBucket])
+    announceWords.innerText = `Unfortunately, there are no available rooms for those search parameters for the selected date (${date.replaceAll('/', '-')}). Please select 'All Rooms' to see all available suites for current date.`
+  }
 
   function toggleElements(array) {
     let toggleElements = array.map(element => element.classList.toggle('hidden'))
     return toggleElements;
+  }
+
+  function showElements(array) {
+    let showElements = array.map(element => element.classList.remove('hidden'))
+    return showElements;
+  }
+
+  function hideElements(array) {
+    let hideElements = array.map(element => element.classList.add('hidden'))
+    return hideElements
+  }
+
+  function resetFilter() {
+    roomFilter = undefined;
+    for (let radioButton of radioBtns) {
+      const checkedButton = radioButton.checked;
+      checkedButton ? roomFilter = radioButton.id : null
+    }
   }
 
   function postRoomBooking(bookingObject) {
